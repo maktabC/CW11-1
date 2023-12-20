3fe7d307e4901a3efee1a9916595d4fb451c814c
Navid Jafarzadeh <navid.jfrzadeh@gmail.com>
2023-12-20 12:09:03 +0330

    add save to file

---
+++ CW11-1/Resturant/Resturant/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Resturant.Entities;
+
+namespace Resturant.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromForm] MyModel model)
+        {
+            string modelContex = $"food name: {model.FoodName}    food type: {model.FoodType}    is available: {model.IsAvailable}     side dishes: {model.haveTomato} {model.havePickledCucumber}";
+
+            System.IO.File.WriteAllText(model.FoodName + ".txt", modelContex);
+
+            return Ok();
+        }
+    }
+
+    public class MyModel
+    {
+        public string? FoodName { get; set; }
+        public string? FoodType { get; set; }
+        public string? IsAvailable { get; set; }
+        public string? haveTomato { get; set; }
+        public string? havePickledCucumber { get; set; }
+    }
+}
---
+++ CW11-1/Resturant/Resturant/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Resturant.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public bool isAvailable { get; set; }
+        public string Category { get; set; }
+    }
+}
